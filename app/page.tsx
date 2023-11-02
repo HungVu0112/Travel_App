4af28c54c11ad8aefe1f77d6171f678249dd971c
HungVu0112 <pcsolaggy123@gmail.com>
2023-11-02 08:51:31 +0700

    Components Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Camp, Hero, Guide, Features, GetApp } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-bold text-teal-500'>
-      Hello World
-    </h1>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
   )
 }
--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,19 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Button from "./Button";
+import Hero from "./Hero";
+import Features from "./Features";
+import GetApp from "./GetApp";
+import Camp from "./Camp";
+import Guide from "./Guide";
 
 export {
     Navbar,
     Footer,
     Button,
+    Hero,
+    Features,
+    GetApp,
+    Camp,
+    Guide,
 }
